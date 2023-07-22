ee40b54c76722bc7fba37dccf5b60fb1f33cfec5
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-07-23 00:06:06 +0600

    Add files via upload

---
+++ String/Input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    cout <<"Enter your name =";
+    char name[20];
+    gets(name);
+    cout << "Welcome to Daffodil International University " << name << endl;
+    return 0;
+}
